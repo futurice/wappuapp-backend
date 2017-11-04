9f75a0f50418dcf60cd94dce94e16399d5fb554b
Pietari Heino <ph@extreg.com>
2017-11-04 22:21:55 +0200

    migraatio ja seedi heiloille; luo yhden käyttäjän ja lisää taulun. users-seediä muokattu, jotta se lisää ensin käyttäjän.

--- seeds/users.js
+++ seeds/users.js
@@ -6,5 +6,11 @@ exports.seed = function(knex, Promise) {
     team_id: 1,
     name: 'Hessu Kypärä',
     uuid: 'hessu'
-  });
+  })
+  .then(() => util.insertOrUpdate(knex, 'users', {
+    id: 2,
+    team_id: 2,
+    name: 'Pate Pat',
+    uuid: '2002'
+  }))
 };
