5849fb269920d62c1eb573e41cbaf2c605ad8b51
Juho Vähätalo <juho.vahatalo@studen.tut.fi>
2017-11-23 01:06:32 +0200

    Trying to fix things

---
+++ src/core/admin-core.js
@@ -0,0 +1,26 @@
+import _ from 'lodash';
+const {knex} = require('../util/database').connect();
+import {GCS_CONFIG} from '../util/gcs';
+//import CONST from '../constants';
+const logger = require('../util/logger')(__filename);
+//import moment from 'moment-timezone';
+
+//Admin deleteFeedItem same as in feed-core.js but doesn't need opts.client.uuid 
+function deleteFeedItem(id) {
+  return knex('feed_items').delete().where({
+    'id': id,
+  })
+  .then(deletedCount => {
+    if (deletedCount > 1) {
+      logger.error('Deleted feed item', id);
+      throw new Error('Unexpected amount of deletes happened: ' + deletedCount)
+    }
+
+    return deletedCount;
+  });
+}
+
+export {
+  deleteFeedItem,
+};
+
--- src/routes.js
+++ src/routes.js
@@ -16,6 +16,7 @@ import * as radioHttp from './http/radio-http';
 import * as wappuMood from './http/wappu-mood-http';
 import * as imageHttp from './http/image-http';
 import * as loginHttp from './http/login-http';
+import * as adminHttp from './http/admin-http';
 
 
 const requireAuth = passport.authenticate('jwt', {session: false});
@@ -38,7 +39,7 @@ function createRouter() {
 
   router.get('/feed', feedHttp.getFeed);
   router.delete('/feed/:id', feedHttp.deleteFeedItem);
-
+  router.delete('/admin/feed/:id', requireAuth, adminHttp.deleteFeedItem);
   router.get('/image/:id', imageHttp.getImage);
 
   router.get('/announcements', announcementHttp.getAnnouncements);
