97ce9290787eb44020d35c863df4024503c5b739
Pietari Heino <ph@extreg.com>
2018-01-14 19:19:39 +0200

    feedback-tauluun uuid käyttäjätietoa varten

--- migrations/20171216170940_add_feedbacks.js
+++ migrations/20171216170940_add_feedbacks.js
@@ -4,13 +4,13 @@ exports.up = function(knex, Promise)
   return knex.schema.createTable('feedback', function(table)
   {
     table.bigIncrements('id').primary().index();
-    
     table.integer('event_id').notNullable().unique().index();
     table.foreign('event_id')
     .references('id')
     .inTable('events')
     .onDelete('RESTRICT')
     .onUpdate('CASCADE');
+    table.string('uuid').notNullable().unique().index();
 
     table.text('feeback_text','longtext');
     table.integer('grade');
