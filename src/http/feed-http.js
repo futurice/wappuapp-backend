b714fbc45fa9c9231123be6b048ac0cc31cfc31c
Mikael Lagerbom <tom.lagerbom@student.tut.fi>
2018-02-02 16:19:17 +0200

    fixing email sending

--- src/http/feed-http.js
+++ src/http/feed-http.js
@@ -1,21 +1,22 @@
-import _ from 'lodash';
-import * as feedCore from '../core/feed-core';
-import {createJsonRoute, throwStatus} from '../util/express';
-import {assert} from '../validation';
-import { create } from 'domain';
-
+import _ from "lodash";
+import * as feedCore from "../core/feed-core";
+import { createJsonRoute, throwStatus } from "../util/express";
+import { assert } from "../validation";
 
 const getFeed = createJsonRoute(function(req, res) {
-  const feedParams = assert({
-    beforeId: req.query.beforeId,
-    limit: req.query.limit,
-    city: req.query.cityId,
-    sort: req.query.sort,
-    type: req.query.type,
-    since: req.query.since,
-    offset: req.query.offset,
-    parent_id: req.query.parent_id
-  }, 'feedParams');
+  const feedParams = assert(
+    {
+      beforeId: req.query.beforeId,
+      limit: req.query.limit,
+      city: req.query.cityId,
+      sort: req.query.sort,
+      type: req.query.type,
+      since: req.query.since,
+      offset: req.query.offset,
+      parent_id: req.query.parent_id
+    },
+    "feedParams"
+  );
 
   const coreParams = _.merge(feedParams, {
     client: req.client
@@ -24,30 +25,27 @@ const getFeed = createJsonRoute(function(req, res) {
 });
 
 const deleteFeedItem = createJsonRoute(function(req, res) {
-  const id = assert(req.params.id, 'common.primaryKeyId');
+  const id = assert(req.params.id, "common.primaryKeyId");
 
-  return feedCore.deleteFeedItem(id, {client: req.client})
-  .then(deletedCount => {
-    if (deletedCount === 0) {
-      // NOTE: deletedCount === 0, might also mean "forbidden" because the uuid
-      // restriction is done in the SQL
-      // In both cases, we just inform the user with Not Found
-      return throwStatus(404, 'Not Found');
-    } else {
-      // Return 200 OK
-      return undefined;
-    }
-  });
+  return feedCore
+    .deleteFeedItem(id, { client: req.client })
+    .then(deletedCount => {
+      if (deletedCount === 0) {
+        // NOTE: deletedCount === 0, might also mean "forbidden" because the uuid
+        // restriction is done in the SQL
+        // In both cases, we just inform the user with Not Found
+        return throwStatus(404, "Not Found");
+      } else {
+        // Return 200 OK
+        return undefined;
+      }
+    });
 });
 
-const refreshCommentNumber = createJsonRoute(function(req, res){
+const refreshCommentNumber = createJsonRoute(function(req, res) {
   const id = req.params.id;
   const client = req.client;
   return feedCore.refreshCommentNumber(id, client);
 });
 
-export {
-  getFeed,
-  deleteFeedItem,
-  refreshCommentNumber
-};
+export { getFeed, deleteFeedItem, refreshCommentNumber };
