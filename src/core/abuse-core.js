3f4bbd1b9eb6919a3ecf2678255b7ae469ddf394
Rudi Ritosalo <rudi.ritosalo@student.tut.fi>
2018-02-04 17:28:25 +0200

    src/core/abuse-core.js

--- src/core/abuse-core.js
+++ src/core/abuse-core.js
@@ -15,37 +15,36 @@ function reportFeedItem(reportObj) {
     'feed_item_id': reportObj.feedItemId
   })
   .then(check =>{
-    console.log(check)
     if (!_.isEmpty(check)){
       throw new Error('User has already reported this message: ' + reportObj.feed_item_id)
     }
     else {
       return knex('users').select('id').where('uuid', reportObj.reportCreatorUuid)
-    .then(users => {
-      if (_.isEmpty(users)) {
-        throw new Error('User not found: ' + reportObj.reportCreatorUuid);
-      }
-      const dbRow = {
-        'feed_item_id': reportObj.feedItemId,
-        'report_creator_id': users[0].id,
-        'report_description': _sanitizeText(reportObj.reportDescription)
-      }
-      return knex('feed_item_reports').insert(dbRow).returning('id')
-        .then(rows => {
-          if (_.isEmpty(rows)) {
-            throw new Error('Feed item report creation failed: ' + dbRow);
-          }
-          return rows.length;
-        })
-        .catch(err => {
-          if (err.constraint === 'feed_item_reports_feed_item_id_foreign') {
-            err.status = 404;
-            err.message = `No such feed item: ${ dbRow.feed_item_id }`;
-          } else if (err.constraint === 'feed_item_reports_report_creator_id_foreign') {
-            err.status = 404;
-            err.message = `No such user: ${ dbRow.report_creator_uuid }`;
-          }
-          throw err;
+      .then(users => {
+        if (_.isEmpty(users)) {
+          throw new Error('User not found: ' + reportObj.reportCreatorUuid);
+        }
+        const dbRow = {
+          'feed_item_id': reportObj.feedItemId,
+          'report_creator_id': users[0].id,
+          'report_description': _sanitizeText(reportObj.reportDescription)
+        }
+        return knex('feed_item_reports').insert(dbRow).returning('id')
+          .then(rows => {
+            if (_.isEmpty(rows)) {
+              throw new Error('Feed item report creation failed: ' + dbRow);
+            }
+            return rows.length;
+          })
+          .catch(err => {
+            if (err.constraint === 'feed_item_reports_feed_item_id_foreign') {
+              err.status = 404;
+              err.message = `No such feed item: ${ dbRow.feed_item_id }`;
+            } else if (err.constraint === 'feed_item_reports_report_creator_id_foreign') {
+              err.status = 404;
+              err.message = `No such user: ${ dbRow.report_creator_uuid }`;
+            }
+            throw err;
         });
     });
     }
@@ -80,18 +79,6 @@ function resolveReport(reportParams) {
 }
 
 function getReportedFeedItems(params){
-
-  return knex('feed_item_reports').select('feed_item_id', 'report_creator_id')
-  .where({
-    'report_creator_id': 1,
-    'feed_item_id': 4
-})
-  .then(check =>{
-    console.log(check)
-    if (!_.isEmpty(check)){
-      throw new Error('User has already reported this message')
-    }
-  })
 
   let whereClause = []
   if (params.beforeId){
