dd1f9e45f7de4e899f1ac8c5ea0bab49cb2b1419
Pietari Heino <ph@extreg.com>
2018-02-06 19:19:26 +0200

    throw from feedback adding

--- src/core/feedback-core.js
+++ src/core/feedback-core.js
@@ -1,8 +1,6 @@
 const {knex} = require('../util/database').connect();
 
 function giveFeedback(feedback) {
-  //console.log('feedbackCore::giveFeedback');
-  //console.log(feedback);
   return knex('events')
     .select('events.id')
     .then(eRows => {
@@ -10,7 +8,7 @@ function giveFeedback(feedback) {
 
       if (eventIds.indexOf(parseInt(feedback.id)) === -1) {
         // event id doesn't exist
-        return -1;
+        return null;
       }
 
       return knex('feedback')
@@ -19,16 +17,8 @@ function giveFeedback(feedback) {
         event_id: feedback.id, // eslint-disable-line
         feeback_text: feedback.text, // eslint-disable-line
         grade: feedback.grade
-       }).then(fRows => {
-         // success
-         return 1;
-       }).catch(err => {
-         // something different went wrong
-         console.log('something went wrong in adding feedback:');
-         console.log(feedback);
-         console.log(err);
-         return 0;
        })
+      .then(fRows => fRows)
     })
 }
 
