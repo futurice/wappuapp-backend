4f30ef5b1facea61557505e16963ad6475da8df2
Pasi Lampinen <plam@fl027.local>
2018-04-17 12:03:27 +0300

    update stream url

--- seeds/radios.js
+++ seeds/radios.js
@@ -1,27 +1,31 @@
 var util = require('../src/util/seeds');
 
-
 exports.seed = function(knex, Promise) {
   const cities = {};
 
-  return knex('cities').select('*')
-  .then(rows => {
-    rows.forEach(city => {
-      cities[city.name] = city.id;
-    });
-  })
-  .then(() => util.insertOrUpdate(knex, 'radios', {
-    id: 1,
-    name: 'Rakkauden Wappuradio',
-    city_id: cities['Tampere'],
-    stream: 'http://stream.wappuradio.fi/wappuradio.mp3',
-    website: 'https://wappuradio.fi/',
-  }))
-  .then(() => util.insertOrUpdate(knex, 'radios', {
-    id: 2,
-    name: 'Radiodiodi',
-    city_id: cities['Otaniemi'],
-    stream: 'http://virta.radiodiodi.fi/stream.mp3',
-    website: 'https://radiodiodi.fi/',
-  }));
-}
+  return knex('cities')
+    .select('*')
+    .then(rows => {
+      rows.forEach(city => {
+        cities[city.name] = city.id;
+      });
+    })
+    .then(() =>
+      util.insertOrUpdate(knex, 'radios', {
+        id: 1,
+        name: 'Rakkauden Wappuradio',
+        city_id: cities['Tampere'],
+        stream: 'http://stream.wappuradio.fi/wappuradio.mp3',
+        website: 'https://wappuradio.fi/',
+      })
+    )
+    .then(() =>
+      util.insertOrUpdate(knex, 'radios', {
+        id: 2,
+        name: 'Radiodiodi',
+        city_id: cities['Otaniemi'],
+        stream: 'https://virta.radiodiodi.fi/radiodiodi-mp3',
+        website: 'https://radiodiodi.fi/',
+      })
+    );
+};
